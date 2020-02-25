5d4e3b76429ce72e575d1f553da3cde3982a2482
spaylor4 <spaylor4@yahoo.com>
2020-02-25 09:41:17 -0500

    Add emoji analysis

--- initial_analysis.py
+++ initial_analysis.py
@@ -13,3 +13,17 @@ with open('adulting_tweets.json') as f:
 # find all emojis
 for tweet in tweets:
     tweet['emojis'] = re.findall(r'[\u263a-\U0001f645]', tweet['text'])
+
+emojis = []
+for tweet in tweets:
+    emojis += tweet['emojis']
+
+emoji_counts = pd.DataFrame({x: emojis.count(x) for x in emojis}.items(),
+                            columns = ['emoji', 'appearances'])
+
+n_emoji_tweets = 0
+for tweet in tweets:
+    if len(tweet['emojis']) > 0:
+        n_emoji_tweets += 1
+
+n_emoji_tweets/len(tweets)
